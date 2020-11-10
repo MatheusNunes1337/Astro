3fcaffc28e5e564b7abff9fe264abc6065fc3873
Matheus Nunes <matheusyagami147@gmail.com>
2020-11-10 03:20:11 -0300

    footer em andamento

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,7 +8,11 @@ export default function Footer() {
 
   return (
     <footer>
-  	  	
-	</footer>
+  	  	<div className="about"></div>
+        <div className="footer">
+            <p className="copyright">&copy; 2020 Astro</p>
+            <p className="icon-attribution">icon made by FreePik from flaticon.com</p>
+        </div>
+	  </footer>
   );
 }
