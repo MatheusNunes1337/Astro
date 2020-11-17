40741ec9033200393c18766028673af050829bee
Matheus Nunes <matheusyagami147@gmail.com>
2020-11-17 14:57:06 -0300

    component de formulário do quiz criado

--- src/pages/quiz.js
+++ src/pages/quiz.js
@@ -1,22 +1,20 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 
+import QuizForm from '../components/quizForm'
 
 import '../assets/css/global.css'
 import '../assets/css/client.css'
 
 export default function Quiz() {
+
+	function handleForm() {
+
+	}
+
 	return (
 		<div className="quiz-bg">
-			<p className="aux">Temporizador</p>
-			<div className="quiz-wrapper">
-				<p className="quiz-question">1. Qual é o maior planeta do sistema solar?</p>
-				<button className="quiz-option">A. Marte</button>
-				<button className="quiz-option">B. Júpiter</button>
-				<button className="quiz-option">C. Saturno</button>
-				<button className="quiz-option">D. Vênus</button>
-			</div>
-			<p className="acertos">Acertos: 02</p>
+			<QuizForm />
 		</div>
 	)	
 }
