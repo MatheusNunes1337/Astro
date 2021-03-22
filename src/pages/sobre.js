5d5c6b3f7a7d527651ea3a7774b2c5aa6918ad64
Matheus Nunes <matheusyagami147@gmail.com>
2021-03-22 15:33:24 -0300

    desenvolvida a página de sobre

---
+++ src/pages/sobre.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+import Header from '../components/header'
+import Footer from '../components/footer'
+
+import '../assets/css/global.css'
+import '../assets/css/client.css'
+
+export default function Sobre() {
+
+  return (
+  	<React.Fragment>
+	    <Header />
+	  	<div className="conteudo">
+	  		<article className="about-content">
+	  			<section>
+	  				  <h1> Sobre o Astro </h1>
+              <p>O astro...</p>
+	  			</section>	
+	  		</article>
+	  	</div>
+		<Footer />
+	</React.Fragment>
+  );
+}
